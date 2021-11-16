2899061cfd2fd6b1e95ee28183056de09d5d08ef
Nikhil <nik.theappideas@gmail.com>
2021-11-16 09:03:30 +0530

    msg

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://admin:admin123@cluster0.jxdqx.mongodb.net/grocery?retryWrites=true&w=majority',{
+mongoose.connect(process.env.MONGO_URI ,{
     useNewURlParser:true,
     useUnifiedTopology:true,
     // useCreateIndex:true
