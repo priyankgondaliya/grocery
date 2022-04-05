c40cf9773c262799a150ad17d6923f401ae74203
Nikhil <nik.theappideas@gmail.com>
2022-04-05 10:21:03 +0530

    error3

--- src/routes/googleAuthRoutes.js
+++ src/routes/googleAuthRoutes.js
@@ -31,42 +31,44 @@ router.get('/callback', passport.authenticate('google', { failureRedirect: '/fai
         const cartSession = req.session.cart;
         console.log('cartSession');
         console.log(cartSession);
-        for (const [key, value] of Object.entries(cartSession)) {
-            // console.log(`${key} ${value}`);
-            var cart = await Cart.findOne({ userId: user.id, vendorId: key});
-            if (!cart) {
-                var cart = new Cart({
-                    userId: user.id,
-                    vendorId: key,
-                    products: []
-                })
-                await cart.save();
-            }
-            if ( value.length != 0 ) {
-                for (let i = 0; i < value.length; i++) {
-                    // console.log(value[i]);
-                    let itemIndex = cart.products.findIndex(p => p.productId == value[i].productId);
-                    
-                    if (itemIndex > -1) {
-                        //product exists in the cart, update the quantity
-                        let productItem = cart.products[itemIndex];
-                        productItem.quantity = value[i].quantity;
-                        // console.log(value[i]);
-                        // productItem.quantity = productItem.quantity + value[i].quantity;
-                        cart.products[itemIndex] = productItem;
-                    } else {
-                        //product does not exists in cart, add new item
-                        cart.products.push({
-                            productId: value[i].productId,
-                            quantity: value[i].quantity,
-                            // price: value[i].totalprice
-                        });
-                    }
+        if (cartSession != undefined) {
+            for (const [key, value] of Object.entries(cartSession)) {
+                // console.log(`${key} ${value}`);
+                var cart = await Cart.findOne({ userId: user.id, vendorId: key});
+                if (!cart) {
+                    var cart = new Cart({
+                        userId: user.id,
+                        vendorId: key,
+                        products: []
+                    })
+                    await cart.save();
                 }
-                // req.session.cart = undefined;
-                cartSession[key] = [];
-                console.log(cartSession);
-                await cart.save();
+                if ( value.length != 0 ) {
+                    for (let i = 0; i < value.length; i++) {
+                        // console.log(value[i]);
+                        let itemIndex = cart.products.findIndex(p => p.productId == value[i].productId);
+                        
+                        if (itemIndex > -1) {
+                            //product exists in the cart, update the quantity
+                            let productItem = cart.products[itemIndex];
+                            productItem.quantity = value[i].quantity;
+                            // console.log(value[i]);
+                            // productItem.quantity = productItem.quantity + value[i].quantity;
+                            cart.products[itemIndex] = productItem;
+                        } else {
+                            //product does not exists in cart, add new item
+                            cart.products.push({
+                                productId: value[i].productId,
+                                quantity: value[i].quantity,
+                                // price: value[i].totalprice
+                            });
+                        }
+                    }
+                    // req.session.cart = undefined;
+                    cartSession[key] = [];
+                    console.log(cartSession);
+                    await cart.save();
+                }
             }
         }
 
